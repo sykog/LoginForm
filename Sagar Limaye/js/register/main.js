775c08d66e9f00e2b4b1135136d0aeb114b2cfa9
Sagar Limaye <sagarl3232@hotmail.com>
2019-11-14 10:20:12 -0500

    phone number and confirm password validations in register page

--- Sagar Limaye/js/register/main.js
+++ Sagar Limaye/js/register/main.js
@@ -1,6 +1,14 @@
+var users = {
+    sagar: "TCS12345",
+    admin: "admin12345"
+};
 var form = document.getElementById("login-form");
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     var fd = new FormData(form);
-    
+    var emailField = document.getElementById("email");
+    if(users[fd.get("user")] != null)
+        alert("Username already exists");
+    if(fd.get("pass") == fd.get("confirm_pass"))
+        alert("Passwords do not match");
 });
